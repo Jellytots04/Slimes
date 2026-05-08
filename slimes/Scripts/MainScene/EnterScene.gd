b48544a6f0d925cf405279832abc8dceae0b7916
Jellytots04 <116292161+Jellytots04@users.noreply.github.com>
2026-05-08 11:40:39 +0100

    Added select audio to opening scene

--- slimes/Scripts/MainScene/EnterScene.gd
+++ slimes/Scripts/MainScene/EnterScene.gd
@@ -1,10 +1,8 @@
 extends Node3D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().process_frame
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- slimes/Scripts/OpeningScene/OpeningScene.gd
+++ slimes/Scripts/OpeningScene/OpeningScene.gd
@@ -3,12 +3,15 @@ extends Node3D
 @onready var start_button: Button = $Camera3D/Control/CanvasLayer/Panel/VBoxContainer/StartButton
 @onready var quit_button: Button = $Camera3D/Control/CanvasLayer/Panel/VBoxContainer/QuitButton
 
+@onready var select_sound: AudioStreamPlayer = get_tree().current_scene.get_node("SelectSound")
+
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
 
 func _on_start_pressed() -> void:
 	# Free all slimes in this scene before switching
+	select_sound.play()
 	for slime in get_tree().get_nodes_in_group("slimes"):
 		slime.queue_free()
 	
@@ -17,4 +20,5 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/PCDebugandTest/PCMainTest.tscn")
 
 func _on_quit_pressed() -> void:
+	select_sound.play()
 	get_tree().quit()
